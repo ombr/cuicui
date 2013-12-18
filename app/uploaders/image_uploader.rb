657ab1b3dcf2956600496aac6ec03c5de25f27b9
Luc Boissaye <luc@boissaye.fr>
2013-12-18 13:46:35 +0100

    cloudinary resize and drop iptc.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,4 +6,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :thumbnail do
     resize_to_fit(50, 50)
   end
+
+  version :full do
+    cloudinary_transformation :transformation => [
+      {:width => 1000, :height => 1000, :crop => :limit, flags: 'progressive'}
+    ]
+  end
 end
