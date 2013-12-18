6c8952561ef7bdfc367786b568792beff8d22db3
Luc Boissaye <luc@boissaye.fr>
2013-12-18 14:11:51 +0100

    Update cloudinary quality.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,7 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   version :full do
     cloudinary_transformation :transformation => [
-      {:width => 1000, :height => 1000, :crop => :limit, flags: 'progressive'}
+      {width: 1200, height: 1200, crop: :limit, quality: 90, flags: 'progressive'}
     ]
   end
 end
