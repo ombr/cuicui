85f20f9d992ee6ca681ce471ff57bc168227f376
Luc Boissaye <luc@boissaye.fr>
2013-12-20 08:53:28 +0100

    Add cache control on assets :-D

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Cuicui::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.static_cache_control = 'public, max-age=2592000'
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
