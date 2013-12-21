3ae6f041901063e45a3c8ca6c02560dba0413266
Luc Boissaye <luc@boissaye.fr>
2013-12-21 15:48:25 +0100

    Add tab_link helper :-D

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
       'alert-notice'
     end
   end
+
+  def tab_link(url, html_options: {})
+    content_tag :li, class: ('active' if current_page?(url)) do
+      link_to url, html_options do
+        yield
+      end
+    end
+  end
 end
