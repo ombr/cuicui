224327c1101fe2f4255ab0f8f526add8e56f5139
Luc Boissaye <luc@boissaye.fr>
2014-01-19 18:12:23 +0100

    Add basic auth with AUTH_NAME and AUTH_PASS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  if ENV['AUTH_NAME'] && ENV['AUTH_PASS']
+    http_basic_authenticate_with name: ENV['AUTH_NAME'], password: ENV['AUTH_PASS']
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:danger] = exception.message
     redirect_to new_user_session_path
