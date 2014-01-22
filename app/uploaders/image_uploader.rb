bd919c474d54ccc9e19e0afc4f7c720b61ba4f14
Luc Boissaye <luc@boissaye.fr>
2014-01-22 14:01:29 +0100

    Let's add jpg conversion :-D

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,13 +6,17 @@ class ImageUploader < CarrierWave::Uploader::Base
   make_private
   eager
 
+  process convert: 'jpg'
+
   version :thumbnail do
     resize_to_fill(200, 100)
+    eager
   end
 
   version :full do
+    eager
     cloudinary_transformation transformation: [
-      { width: 1500, height: 1200, crop: :limit, quality: 80, flags: 'progressive' }
+      { width: 1600, height: 1400, crop: :limit, quality: 80, flags: 'progressive' }
     ]
   end
 end
