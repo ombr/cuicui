a2351ecddd1b297afc48917fedc1d862e65f8a6e
Luc Boissaye <luc@boissaye.fr>
2014-02-03 19:40:57 +0100

    Fix migration before rolling out the release :-D

--- db/migrate/20140130124752_add_description_html_to_pages.rb
+++ db/migrate/20140130124752_add_description_html_to_pages.rb
@@ -1,5 +1,5 @@
 class AddDescriptionHtmlToPages < ActiveRecord::Migration
   def change
-    add_column :pages, :description_html, :text
+    add_column :pages, :description_html, :text, default: ''
   end
 end
