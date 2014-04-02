03a1d3355f86e7e12f08561d1f716f7d4ad68bdc
Luc <luc@boissaye.fr>
2014-04-02 20:18:32 +0200

    Add reset password on site edit.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,10 @@ class RegistrationsController < Devise::RegistrationsController
     return redirect_to new_user_session_path if User.count > 0
     super
   end
+
+  private
+
+  def after_update_path_for(ressource)
+    admin_path
+  end
 end
