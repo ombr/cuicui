c958565049b5ba1add9d9a5eec5550a5fad7e7ae
Luc <luc@boissaye.fr>
2014-05-18 12:09:08 +0200

    Css cursor + first draft of next page.

---
+++ app/helpers/images_helper.rb
@@ -0,0 +1,14 @@
+module ImagesHelper
+  def link_to_image(image, options = {})
+    options[:class] ||= ''
+    options[:class] += ' scroll'
+    options[:'data-scroll'] = "#image-#{image.higher_item.id}" 
+    link_to s_image_path(id: image, page_id: image.page), options do
+      if block_given?
+        yield
+      else
+        image.title
+      end
+    end
+  end
+end
