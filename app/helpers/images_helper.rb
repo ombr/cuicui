9f18376c53cdeedb2a57df6632b1c7a4c161168a
Luc <luc@boissaye.fr>
2014-05-18 12:23:31 +0200

    Fix image helper.

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -2,7 +2,7 @@ module ImagesHelper
   def link_to_image(image, options = {})
     options[:class] ||= ''
     options[:class] += ' scroll'
-    options[:'data-scroll'] = "#image-#{image.higher_item.id}" 
+    options[:'data-scroll'] = "#image-#{image.id}"
     link_to s_image_path(id: image, page_id: image.page), options do
       if block_given?
         yield
