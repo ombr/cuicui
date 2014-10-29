c0e48ecb1ba6713575cccd87559de37589c29297
Luc <luc@boissaye.fr>
2014-10-29 11:40:07 +0100

    Fix tests.

--- spec/views/layouts/_application_without_nav.html.haml_spec.rb
+++ spec/views/layouts/_application_without_nav.html.haml_spec.rb
@@ -3,12 +3,28 @@ require 'spec_helper'
 describe 'layouts/_application_without_nav.html.haml' do
 
   it 'render font_body' do
+    FakeWeb.register_uri(
+      :get,
+      "https://www.googleapis.com/webfonts/v1/webfonts?key=#{ENV['GOOGLE_FONT_KEY']}",
+      body: File.read(
+        Rails.root.join('spec', 'fixtures', 'google_webfonts.json')
+      ),
+      content_type: 'application/json'
+    )
     assign(:site, create(:site, font_body: 'Lobster'))
     render
     Capybara.string(rendered).should have_content('Lobster')
   end
 
   it 'render font_header' do
+    FakeWeb.register_uri(
+      :get,
+      "https://www.googleapis.com/webfonts/v1/webfonts?key=#{ENV['GOOGLE_FONT_KEY']}",
+      body: File.read(
+        Rails.root.join('spec', 'fixtures', 'google_webfonts.json')
+      ),
+      content_type: 'application/json'
+    )
     assign(:site, create(:site, font_header: 'Open Sans'))
     render
     Capybara.string(rendered).should have_content('Open Sans')
