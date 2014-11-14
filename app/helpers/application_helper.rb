7f1bec747a8faab2f1fff2eb094d58bca1e45a37
Luc <luc@boissaye.fr>
2014-11-14 11:17:21 +0100

    Evlaa marketing.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
     root_path
   end
 
-  def tab_link(url, html_options: {})
+  def tab_link(url, html_options = {})
     content_tag :li, class: ('active' if current_page?(url)) do
       link_to url, html_options do
         yield
