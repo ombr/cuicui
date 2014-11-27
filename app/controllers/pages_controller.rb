a4bf8ce0d63600338d146f5b02cbecc302c5139a
Luc <luc@boissaye.fr>
2014-11-27 16:14:40 +0100

    Add legal informations on sites.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
 # PagesController
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
-  layout 'admin'
+  layout :layout_for_page
+
+  before_action :load_site_from_host_for_legal
+
+  def load_site_from_host_for_legal
+    return unless params[:id] == 'legal'
+    load_site_from_host
+  end
+
+  def layout_for_page
+    return 'application' if @site && params[:id] == 'legal'
+    'admin'
+  end
 
   rescue_from ActionView::MissingTemplate do |_exception|
     redirect_to root_path
